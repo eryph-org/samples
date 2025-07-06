90fd232eaafa9dd2e21077e86d9422817d567b47
Frank Wagner <frank2568@gmail.com>
2025-07-06 11:24:09 +0200

    sqlserver: use invoke and log scan modules

--- sqlserver/README.md
+++ sqlserver/README.md
@@ -7,7 +7,7 @@ This example shows how to create a catlet that automatically installs SQL Server
 
 1. Clone this repository if you have not already done so.
 2. Download a SQL Server installation ISO, e.g. from my.visualstudio.com
-3. Run the `build-sqlserver.ps1' script and set the arguments to the catlet name and ISO path.
+3. Run the `build-sqlserver.ps1` script and set the arguments to the catlet name and ISO path.
 
 ## How it works
 
@@ -17,4 +17,6 @@ See also the SQLServerDSC: https://github.com/dsccommunity/SqlServerDsc
 If the installation requires a reboot, the script will return 1003 to request that it be run again at the next boot.
 The result of the installation is written to log files that can be retrieved later. 
 
-Finally, the firewall will enable ping replies so that the build-sqlserver script can ping the host. Once this happens, it will connect via Powershell Remoting and check the files written for installation status.
+The script `build-sqlserver.ps1` waits for this status file via winrm. In case
+of a error it tries to fetch generated log files. 
+You can use same pattern to fetch status for any DSC installation.
--- sqlserver/build_sqlserver.ps1
+++ sqlserver/build_sqlserver.ps1
@@ -15,6 +15,38 @@ param (
 )
 
 $ErrorActionPreference = 'Stop'
+Import-Module -Name "$PSScriptRoot/../modules/Eryph.InvokeCommand.psm1"
+Import-Module -Name "$PSScriptRoot/../modules/CloudInit.Analyzers.psm1"
+
+function Get-AdminCredentials {
+    Write-Host
+    Write-Host "Please provide username and password for admin user. The password must meet Windows Server password rules (at least 8 characters and must contain upper case, lower case and digits)." -ForegroundColor Yellow
+    $username = Read-Host "Username for admin"
+    
+    do {
+        $password = Read-Host "admin password (hidden)" -AsSecureString
+        $confirmPassword = Read-Host "Please confirm the password (hidden)" -AsSecureString
+
+        if (([Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))) -ne ([Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($confirmPassword)))) {
+            Write-Host "Passwords do not match. Please try again." -ForegroundColor Red
+            continue
+        }
+
+        $plainPassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+        if ($plainPassword.Length -lt 8 -or `
+            -not ($plainPassword -match '[A-Z]') -or `
+            -not ($plainPassword -match '[a-z]') -or `
+            -not ($plainPassword -match '\d')) {
+            Write-Host "Password does not meet Windows Server password rules. Please try again." -ForegroundColor Red
+            continue
+        }
+
+        break
+    } while ($true)
+
+    $adminCred = New-Object System.Management.Automation.PSCredential($username, $password)
+    return $adminCred
+}
 
 if (-not [System.IO.Path]::IsPathRooted($SQLServerISO)) {
     $SQLServerISO = (Join-Path $PSScriptRoot $SQLServerISO)
@@ -26,13 +58,16 @@ if(-not (Test-Path $SQLServerISO)){
 }
 
 if (-not $Credentials) {
-    $Credentials = Get-Credential -Message "Please provide username and password for your new catlet. The password must meet Windows Server password rules (at least 8 characters and must contain upper case, lower case and digits)."
+    $Credentials = Get-AdminCredentials
 }
 
 Write-Information "Removing existing catlet (if it exists)..." -InformationAction Continue
 Get-Catlet | Where-Object Name -eq $catletName | Remove-Catlet -Force
 
 Write-Information "Booting catlet..." -InformationAction Continue
+
+# the catlet yaml contains a placeholder for the SQL Server ISO path
+# which will be replaced with the actual path to the ISO file
 $catletConfig = Get-Content -Raw -Path (Join-Path $PSScriptRoot "sqlserver.yaml")
 $catletConfig = $catletConfig.Replace("{{ sqlserver_iso }}", $SQLServerISO)
 
@@ -47,52 +82,52 @@ $catlet = New-Catlet `
 
 Start-Catlet -Id $catlet.Id -Force
 
-Write-Information "Waiting 3 minutes..." -InformationAction Continue
+Write-Information "Waiting 3 minutes for bootstrapping..." -InformationAction Continue
 Start-Sleep -Seconds 180
 
 $ipInfo = Get-CatletIp -Id $catlet.Id
 $ip = $ipInfo.IpAddress
 
-do {
-    Write-Information "Waiting for connection..." -InformationAction Continue
-    Start-Sleep -Seconds 10
-    $ping = Test-Connection -ComputerName $ip -Count 1 -Quiet
-} until ($ping)
-
-$opt = New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck
-$session = new-PSSession -ComputerName $ip -Credential $Credentials -UseSSL -Authentication Basic -SessionOption $opt
-Invoke-Command -Session $session -scriptblock {
+# when catlet is booted the fodder will create a file c:\SQLInstallStatus.txt
+# with the content "finished" when the SQL Server installation is done
+# or "failed" if the installation failed
+# the fodder will also create a log file c:\SQLInstall.log with the installation log
+Write-Information "Waiting for SQLServer installation to finish..." -InformationAction Continue
+Invoke-CommandWinRM -ComputerName $ip -Credentials $Credentials -Retry -scriptblock {
 
     $finished = Test-Path c:\SQLInstallStatus.txt
-
     if ($finished) { return }
-
     do {
-        Write-Information "Waiting for SQLServer installation to finish..." -InformationAction Continue
         Start-Sleep -Seconds 10
         $finished = Test-Path c:\SQLInstallStatus.txt
     } until ($finished)
     
 }
 
-$status = Invoke-Command -Session $session -scriptblock {
+# the fodder will also create a log file c:\SQLInstall.log with the installation log
+$status = Invoke-CommandWinRM -ComputerName $ip -Credentials $Credentials -Retry -TimeoutInSeconds 60 -scriptblock {
     return Get-Content c:\SQLInstallStatus.txt
 }
 
 if ($status -eq "failed") {
     Write-Error "SQL Server installation failed"
 
-    $logContent = Invoke-Command -Session $session -scriptblock {
+    # for error handling we both read the SQLInstall.log and the cloudbase-init log
+    Write-Information "Reading SQL Server installation log..." -InformationAction Continue
+    $logContent = Invoke-CommandWinRM -ComputerName $ip -Credentials $Credentials -Retry -TimeoutInSeconds 60 -scriptblock {
         return Get-Content c:\SQLInstall.log
     }
     
-    Write-Host $logContent
+    Write-Output $logContent
+
+    Write-Information "Reading cloudbase-init log..." -InformationAction Continue
+    $logContent = Invoke-CommandWinRM -ComputerName $ip -Credentials $Credentials -Retry -TimeoutInSeconds 60 -scriptblock {
+        return Get-Content "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\log\cloudbase-init.log"
+    }
+    $logContent | Get-CloudbaseInitUserDataError
 
     exit
 }
 
-Write-Information "Catlet $catletname is ready for testing" -InformationAction Continue
+Write-Information "Catlet $catletname is ready..." -InformationAction Continue
 
-# run Tests
-
-Remove-PSSession -Session $session
